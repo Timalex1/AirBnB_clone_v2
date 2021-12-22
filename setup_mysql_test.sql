e17fc93568aafb54c43f687e2e8433c976e3f827
root <root@DESKTOP-F2BLBCH.localdomain>
2021-12-23 00:09:10 +0300

    sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- script to create and prepare test db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER If NOT EXISTS hbnb_test@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+ GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test@localhost';
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test@localhost';
+FLUSH PRIVILEGES;
